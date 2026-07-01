fa868fb1fc78791fe1b49469556d40d89d1a0fdf
Alex <62323755+AlexShade909@users.noreply.github.com>
2026-07-02 00:55:37 +0300

    убрал мусор из коментариев

--- main.go
+++ main.go
@@ -23,137 +23,115 @@ type Dog struct {
 	age            int
 	weightKg       int
 	checkInDate    string
-	stayShelerName string
+	stayShelerName Shelter
 }
 
 func (d Dog) printDogInfo() {
 
-	println("Кличка:", d.nickname)
-	println("Номер в базе:", d.numberInBase)
-	println("Возраст, лет:", d.age)
-	println("Вес, кг:", d.weightKg)
-	println("Когда попал в приют:", d.checkInDate)
-	println("К какому шелтеру относится:", d.stayShelerName)
+	fmt.Println("Кличка:", d.nickname)
+	fmt.Println("Номер в базе:", d.numberInBase)
+	fmt.Println("Возраст, лет:", d.age)
+	fmt.Println("Вес, кг:", d.weightKg)
+	fmt.Println("Когда попал в приют:", d.checkInDate)
+	fmt.Println("К какому шелтеру относится:", d.stayShelerName.nameShelter)
 
 }
 
 func (d Shelter) printShelterInfo() {
-	println(d.nameShelter)
-	println(d.address)
-	println(d.contactNumber)
-	println(d.workingTime)
+	fmt.Println(d.nameShelter)
+	fmt.Println(d.address)
+	fmt.Println(d.contactNumber)
+	fmt.Println(d.workingTime)
 }
 
 func main() {
 
-	Shelter1 := Shelter{
+	var sliceDog []Dog
+	var sliceShelter []Shelter
+
+	sliceShelter = append(sliceShelter, Shelter{
 		nameShelter:   "Приют 'Шанс'",
 		address:       "Пятруся Глебки 17",
 		contactNumber: "+375 29 511-22-13",
 		workingTime:   "10:00 - 22:00",
-	}
+	})
 
-	Shelter2 := Shelter{
+	sliceShelter = append(sliceShelter, Shelter{
 		nameShelter:   "Приют 'Привет'",
 		address:       "Мстислава Чудотворца 4/1",
 		contactNumber: "+375 12 544-65-45",
 		workingTime:   "11:00 - 21:15",
-	}
+	})
 
-	Dog1 := Dog{
+	sliceDog = append(sliceDog, Dog{
 		numberInBase:   1,
 		nickname:       "Чарли",
 		age:            3,
 		weightKg:       7,
 		checkInDate:    "05.02.2025",
-		stayShelerName: Shelter1.nameShelter,
-	}
+		stayShelerName: sliceShelter[0],
+	})
 
-	Dog2 := Dog{
+	sliceDog = append(sliceDog, Dog{
 		numberInBase:   2,
 		nickname:       "Вил",
 		age:            4,
 		weightKg:       17,
 		checkInDate:    "15.03.2025",
-		stayShelerName: Shelter2.nameShelter,
-	}
-	Dog3 := Dog{
+		stayShelerName: sliceShelter[1],
+	})
+	sliceDog = append(sliceDog, Dog{
 		numberInBase:   3,
 		nickname:       "Кайман",
 		age:            2,
 		weightKg:       3,
 		checkInDate:    "25.11.2022",
-		stayShelerName: Shelter1.nameShelter,
-	}
+		stayShelerName: sliceShelter[1],
+	})
+	// fmt.Println(sliceDog[0].numberInBase)
+	// fmt.Println(sliceDog[0].nickname)
+	// fmt.Println(sliceDog[0].age)
+	// fmt.Println(sliceDog[0].weightKg)
+	// fmt.Println(sliceDog[0].checkInDate)
+	// fmt.Println(sliceDog[0].stayShelerName.nameShelter)
+	// fmt.Println(sliceShelter[0].address)
+	// fmt.Println(sliceShelter[0].contactNumber)
+	// fmt.Println(sliceShelter[0].nameShelter)
+	// fmt.Println(sliceShelter[0].workingTime)
+	// sliceDog[0].printDogInfo()
+	// sliceShelter[0].printShelterInfo()
+	varChooseNameDog := 0
+	varPickUpDog := 0
 
 	for {
-		varChooseNameDog := 0
-		varPickUpDog := 0
-
 		clearConsole()
 
-		println("Собаки, которых вы могли бы забрать из приюта\nЧтобы узнать больше о питомце введите его номер")
-		println(Dog1.numberInBase, Dog1.nickname)
-		println(Dog2.numberInBase, Dog2.nickname)
-		println(Dog3.numberInBase, Dog3.nickname)
+		fmt.Println("Собаки, которых вы могли бы забрать из приюта\nЧтобы узнать больше о питомце введите его номер")
+		fmt.Println(sliceDog[0].numberInBase, sliceDog[0].nickname)
+		fmt.Println(sliceDog[1].numberInBase, sliceDog[1].nickname)
+		fmt.Println(sliceDog[2].numberInBase, sliceDog[2].nickname)
 
 		println("Введите номер собаки, о которой хотите узнать больше:\n")
 		fmt.Scan(&varChooseNameDog)
-
+		varChooseNameDog = varChooseNameDog - 1
 		clearConsole()
 
-		if varChooseNameDog == Dog1.numberInBase {
-			Dog1.printDogInfo()
+		sliceDog[varChooseNameDog].printDogInfo()
 
-			println()
-			println()
-			println()
-			println("Забрать из приюта?\n1 Да, забрать из приюта\n2 Нет, смотреть других собак")
-			fmt.Scan(&varPickUpDog)
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println("Забрать из приюта?\n1 Да, забрать из приюта\n2 Нет, смотреть других собак")
+		fmt.Scan(&varPickUpDog)
 
-			if varPickUpDog == 1 {
-				clearConsole()
+		if varPickUpDog == 1 {
+			clearConsole()
 
-				Shelter1.printShelterInfo()
+			sliceShelter[varChooseNameDog].printShelterInfo()
 
-				break
-
-			}
+			break
 
 		}
-		if varChooseNameDog == Dog2.numberInBase {
-			Dog2.printDogInfo()
-
-			println()
-			println()
-			println()
-			println("Забрать из приюта?\n1 Да, забрать из приюта\n2 Нет, смотреть других собак")
-			fmt.Scan(&varPickUpDog)
-
-			if varPickUpDog == 1 {
-				clearConsole()
-				Shelter2.printShelterInfo()
-				break
-
-			}
-		}
-		if varChooseNameDog == Dog3.numberInBase {
-			Dog3.printDogInfo()
-
-			println()
-			println()
-			println()
-			println("Забрать из приюта?\n1 Да, забрать из приюта\n2 Нет, смотреть других собак")
-			fmt.Scan(&varPickUpDog)
-
-			if varPickUpDog == 1 {
-				clearConsole()
-				Shelter1.printShelterInfo()
-				break
-
-			}
-		}
-
 	}
 
 }
