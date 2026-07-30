6368a7db77d081ae22bc0e154bcbd1bd79d331ea
Alex <62323755+AlexShade909@users.noreply.github.com>
2026-07-30 15:45:51 +0300

    добавлены функции scenarioTakeDog scenarioAddDog

--- main.go
+++ main.go
@@ -2,66 +2,12 @@ package main
 
 import "fmt"
 
-//func clearConsole() {
-//	fmt.Print("\033[H\033[2J")
-//	println()
-//	println()
-//}
-
-//func (s Shelter) printShelterInfo() {
-//	fmt.Println(s.name)
-//	fmt.Println(s.address)
-//	fmt.Println(s.phoneNumber)
-//	fmt.Println(s.workingTime)
-//}
-
-//func inputChooseNameDog(lenSliceDog int) (intChooseNameDog int) {
-//	for {
-//		_, err := fmt.Scan(&intChooseNameDog)
-//		if err != nil {
-//			fmt.Println("Ошибка ввода")
-//			continue
-//		}
-//		if intChooseNameDog > 0 && intChooseNameDog <= lenSliceDog {
-//			break
-//		} else {
-//			fmt.Println("Ошибка диапазона")
-//		}
-//
-//	}
-//	return intChooseNameDog
-//}
-
-//func inputPickUpDog() (intPickUpDog int) {
-//	for {
-//		_, err := fmt.Scan(&intPickUpDog)
-//		if err != nil {
-//			fmt.Println("Ошибка ввода")
-//			continue
-//		}
-//		if intPickUpDog != 1 && intPickUpDog != 2 {
-//			fmt.Println("Ошибка диапазона")
-//		} else {
-//			break
-//		}
-//	}
-//	return intPickUpDog
-//}
-
 func main() {
 
 	policlinics := CreatePoliclinics()
 	shelters := CreateShelters()
 	dogs := CreateDogs(shelters, policlinics)
 
-	printDogInfo(dogs, "Вил")
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	printShelterInfo(&shelters[1])
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	printDogList(dogs)
-
+	fmt.Println(dogs)
+	scenarioTakeDog()
 }
--- scenarios.go
+++ scenarios.go
@@ -1,3 +1,31 @@
 package main
 
-//сценарии (scenarioAddDog, scenarioTakeDog) + runMenu
+import "fmt"
+
+// возвращает false, если пользователь решил закончить программу
+func scenarioTakeDog(dogs map[string]*Dog, shelters []Shelter, policlinics []Policlinic) bool {
+	printDogList(dogs)
+	nickname := readNonEmptyString("Введите кличку собаки: ")
+
+	d, ok := FindDog(dogs, nickname)
+	if !ok {
+		fmt.Println("Собака с такой кличкой не найдена")
+		return true // не выходим из программы, просто не нашли
+	}
+
+	printDogInfo(dogs, nickname)
+	if readYesNo("Забрать из приюта? (да/нет): ") {
+		RemoveDog(dogs, nickname)
+		fmt.Println("Собака удалена из общего списка, приюта и поликлиники")
+		printShelterInfo(d.shelter)
+	}
+
+	return readYesNo("Смотреть ещё? (да/нет): ")
+}
+
+func scenarioAddDog(dogs map[string]*Dog, shelters []Shelter, policlinics []Policlinic) bool {
+	// считать nickname/age/weight/checkInDate
+	// дать выбрать приют и поликлинику из списка (readMenuChoice по индексу)
+	// AddDog(...) + печать сообщений на каждом шаге
+	return readYesNo("Смотреть ещё? (да/нет): ")
+}
