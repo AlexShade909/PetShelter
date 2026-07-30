9067ab810b4b3a76790416c495c79063e2ea459f
Alex <62323755+AlexShade909@users.noreply.github.com>
2026-07-30 17:11:51 +0300

    добален функционал "Добавить собаку" Теперь программа работает в цикле пока пользователь сам не выйдет

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ func main() {
 	policlinics := CreatePoliclinics()
 	shelters := CreateShelters()
 	dogs := CreateDogs(shelters, policlinics)
+	flag := true
+	for flag {
 
-	choice := readMenuChoice("1. Выбрать собаку\n2. Добавить собаку\n3. Выход\n ", 1, 3)
-	switch choice {
-	case 1:
-		fmt.Println("Выбрать собаку")
-		for scenarioTakeDog(dogs, shelters, policlinics) {
+		choice := readMenuChoice("1. Выбрать собаку\n2. Добавить собаку\n3. Выход\n ", 1, 3)
+		switch choice {
+		case 1:
+			fmt.Println("Выбрать собаку")
+			flag = scenarioTakeDog(dogs, shelters, policlinics)
+		case 2:
+			fmt.Println("Добавить собаку")
+			flag = scenarioAddDog(dogs, shelters, policlinics)
+
+		case 3:
+			fmt.Println("Выход")
+			return
+
+		default:
+			fmt.Println("неизвестная команда")
+			//такой вариант исключён логикой функции readMenuChoice
 		}
-	case 2:
-		fmt.Println("Добавить собаку")
-		scenarioAddDog(dogs, shelters, policlinics)
-		return
-
-	case 3:
-		fmt.Println("Выход")
-		return
-
-	default:
-		fmt.Println("неизвестная команда")
-		//такой вариант исключён логикой функции readMenuChoice
 	}
-
 }
