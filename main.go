4193d7af721ca1af8ab7d6bff7a45d6b647ab566
Alex <62323755+AlexShade909@users.noreply.github.com>
2026-08-21 11:51:08 +0300

    main.go - fix initialise maps policlinicStorage.tables, sheltersStorage.table

--- main.go
+++ main.go
@@ -3,57 +3,59 @@ package main
 import "fmt"
 
 func main() {
-	policlinicsStorage := PoliclinicsStorage{}
+	policlinicsStorage := PoliclinicsStorage{
+		table: make(map[string]Policlinic),
+	}
 	policlinicsStorage.Add(CreatePoliclinics()...)
 
-	sheltersStorage := SheltersStorage{}
+	sheltersStorage := SheltersStorage{
+		table: make(map[string]Shelter),
+	}
 	sheltersStorage.Add(CreateShelters()...)
 
-	fmt.Println(sheltersStorage.Find("1"))
-	fmt.Println("Remove")
-	sheltersStorage.Remove("1")
-	fmt.Println(sheltersStorage.Find("1"))
+	dogs := CreateDogs()
 
+	policlinnic1 := policlinicsStorage.Find("Поликлиника 1")
+	policlinnic2 := policlinicsStorage.Find("Поликлиника 2")
+	shelter1 := sheltersStorage.Find("Шелтер 1")
+	shelter2 := sheltersStorage.Find("Шелтер 2")
+
+	policlinnic1.AddDogs(dogs[0], dogs[1])
+	policlinnic2.AddDogs(dogs[2])
+
+	shelter1.AddDogs(dogs[0], dogs[2])
+	shelter2.AddDogs(dogs[1])
+
+	fmt.Println(policlinnic1.listPatients)
+	fmt.Println(policlinnic2.listPatients)
+	fmt.Println(shelter1.listPets)
+	fmt.Println(shelter2.listPets)
 	/*
 
-		dogs := CreateDogs()
+		facade := FacadeStorage{
+			PoliclinicsStorage: policlinicsStorage,
+			SheltersStorage:    sheltersStorage,
+		}
 
-		policlinnic1 := policlinicsStorage.Find("1")
-		policlinnic2 := policlinicsStorage.Find("2")
-		shelter1 := sheltersStorage.Find("1")
-		shelter2 := sheltersStorage.Find("2")
+		flag := true
 
-		policlinnic1.AddDogs(dogs[0], dogs[1])
-		policlinnic2.AddDogs(dogs[2])
-
-		shelter1.AddDogs(dogs[0], dogs[2])
-		shelter2.AddDogs(dogs[1])
-
-
-			facade := FacadeStorage{
-				PoliclinicsStorage: policlinicsStorage,
-				SheltersStorage:    sheltersStorage,
+		for flag {
+			choice := readMenuChoice("1. Выбрать собаку\n2. Добавить собаку\n3. Выход\n ", 1, 3)
+			switch choice {
+			case 1:
+				fmt.Println("Выбрать собаку")
+				flag = scenarioTakeDog(facade)
+			case 2:
+				fmt.Println("Добавить собаку")
+				flag = scenarioAddDog(facade)
+			case 3:
+				fmt.Println("Выход")
+				return
+			default:
+				fmt.Println("неизвестная команда")
+				//такой вариант исключён логикой функции readMenuChoice
 			}
-
-			flag := true
-
-			for flag {
-				choice := readMenuChoice("1. Выбрать собаку\n2. Добавить собаку\n3. Выход\n ", 1, 3)
-				switch choice {
-				case 1:
-					fmt.Println("Выбрать собаку")
-					flag = scenarioTakeDog(facade)
-				case 2:
-					fmt.Println("Добавить собаку")
-					flag = scenarioAddDog(facade)
-				case 3:
-					fmt.Println("Выход")
-					return
-				default:
-					fmt.Println("неизвестная команда")
-					//такой вариант исключён логикой функции readMenuChoice
-				}
-			}
+		}
 
 	*/
 }
